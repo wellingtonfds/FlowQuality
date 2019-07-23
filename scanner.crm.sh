8b61eb80642d2cbe176079905a67080f0a620adc
Wellington <wellington@gmail.com>
2019-07-23 11:17:40 -0300

    add new CRM Project

--- scanner.crm.sh
+++ scanner.crm.sh
@@ -1,4 +1,4 @@
-cd $HOME/Apps/spm
+cd $HOME/Apps/crm
 git pull
 cd ..
 ./FlowQuality/sonar-scanner/bin/sonar-scanner -Dproject.settings=$HOME/Apps/FlowQuality/sonar-scanner/conf/sonar-scanner.crm.properties
